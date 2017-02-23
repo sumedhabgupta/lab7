f61ed1f2d25f77ff86aa1501a883249a0f36a1b2
Sumedha Gupta <sumedhabgupta@gmail.com>
2017-02-23 10:34:46 -0800

    finished sg 7

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,11 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(req, res){
-	projects["min"] = false; 
+	projects['min'] = false; 
   	res.render('index', projects);
 };
 
 exports.viewMin = function(req, res){
-	projects["min"] = true; 
+	projects['min'] = true; 
 	res.render('index', projects); 
 }
